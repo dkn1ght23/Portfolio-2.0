592cb1a4a79ba8d97f170e9a5b17cb2146804423
dkn1ght23 <mujammal.salman@gmail.com>
2022-05-12 23:39:20 +0600

    header

---
+++ src/app/root-browser/components/root-intro/root-intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root-intro',
+  templateUrl: './root-intro.component.html',
+  styleUrls: ['./root-intro.component.scss']
+})
+export class RootIntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
